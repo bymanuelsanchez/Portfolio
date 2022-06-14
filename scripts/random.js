33c05d506967ad7fa0bd70585bc468a577e1a81e
Manuel Sánchez <bymanuelsanchez@gmail.com>
2022-06-14 19:20:21 +0200

    js done

--- scripts/random.js
+++ scripts/random.js
@@ -1,7 +1,132 @@
 const mateo = document.querySelector(".rg-mateo");
+const mona = document.querySelector(".rg-mona");
+const coco = document.querySelector(".rg-coco");
+const desorden = document.querySelector(".rg-desorden");
+const lyquido = document.querySelector(".rg-lyquido");
+const lugar = document.querySelector(".rg-lugar");
+const wurli = document.querySelector(".rg-wurli");
+const parla = document.querySelector(".rg-parla");
+const pop = document.querySelector(".rg-pop");
+const loco = document.querySelector(".rg-loco");
+const meseta = document.querySelector(".rg-meseta");
+
 const img = document.querySelector(".random-img");
 
-mateo.addEventListener("click", func1);
+
+
+mateo.addEventListener("mouseover", func1, true);
 function func1 () {
     img.classList.add("mateo-img"); 
+}
+mateo.addEventListener("mouseout", func2, true);
+function func2 () {
+    img.classList.remove("mateo-img"); 
+}
+
+
+
+mona.addEventListener("mouseover", func3, true);
+function func3 () {
+    img.classList.add("mona-img"); 
+}
+mona.addEventListener("mouseout", func4, true);
+function func4 () {
+    img.classList.remove("mona-img"); 
+}
+
+
+
+coco.addEventListener("mouseover", func5, true);
+function func5 () {
+    img.classList.add("coco-img"); 
+}
+coco.addEventListener("mouseout", func6, true);
+function func6 () {
+    img.classList.remove("coco-img"); 
+}
+
+
+
+desorden.addEventListener("mouseover", func7, true);
+function func7 () {
+    img.classList.add("desorden-img"); 
+}
+desorden.addEventListener("mouseout", func8, true);
+function func8 () {
+    img.classList.remove("desorden-img"); 
+}
+
+lyquido.addEventListener("mouseover", func9, true);
+function func9 () {
+    img.classList.add("lyquido-img"); 
+}
+lyquido.addEventListener("mouseout", func10, true);
+function func10 () {
+    img.classList.remove("lyquido-img"); 
+}
+
+
+
+lugar.addEventListener("mouseover", func11, true);
+function func11 () {
+    img.classList.add("lugar-img"); 
+}
+lugar.addEventListener("mouseout", func12, true);
+function func12 () {
+    img.classList.remove("lugar-img"); 
+}
+
+
+
+wurli.addEventListener("mouseover", func11, true);
+function func11 () {
+    img.classList.add("wurli-img"); 
+}
+wurli.addEventListener("mouseout", func12, true);
+function func12 () {
+    img.classList.remove("wurli-img"); 
+}
+
+
+
+parla.addEventListener("mouseover", func13, true);
+function func13 () {
+    img.classList.add("parla-img"); 
+}
+parla.addEventListener("mouseout", func14, true);
+function func14 () {
+    img.classList.remove("parla-img"); 
+}
+
+
+
+pop.addEventListener("mouseover", func15, true);
+function func15 () {
+    img.classList.add("pop-img"); 
+}
+pop.addEventListener("mouseout", func16, true);
+function func16 () {
+    img.classList.remove("pop-img"); 
+}
+
+
+
+loco.addEventListener("mouseover", func17, true);
+function func17 () {
+    img.classList.add("loco-img"); 
+}
+loco.addEventListener("mouseout", func18, true);
+function func18 () {
+    img.classList.remove("loco-img"); 
+}
+
+
+
+meseta.addEventListener("mouseover", func19, true);
+function func19 () {
+    img.classList.add("meseta-img"); 
+}
+meseta.addEventListener("mouseout", func20, true);
+function func20 () {
+    img.classList.remove("meseta-img"); 
 }
