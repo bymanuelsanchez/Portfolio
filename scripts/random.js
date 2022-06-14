57c208394520d6099a186343b9300b798e1321f6
Manuel Sánchez <bymanuelsanchez@gmail.com>
2022-06-14 07:59:46 +0200

    Falta JS

---
+++ scripts/random.js
@@ -0,0 +1,7 @@
+const mateo = document.querySelector(".rg-mateo");
+const img = document.querySelector(".random-img");
+
+mateo.addEventListener("click", func1);
+function func1 () {
+    img.classList.add("mateo-img"); 
+}
